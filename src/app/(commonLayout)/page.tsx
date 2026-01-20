8a6cdf926dfa82840e2687a4b3d38bf85ce6750d
ar-saad <arsaad.dev@gmail.com>
2026-01-20 11:44:49 +0600

    feat: integrate better-auth for authentication, update login/register forms ui and implement login with google option

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
-export default function Home() {
+export default async function Home() {
+  const session = await authClient.getSession();
+  console.log(session);
+
   return (
     <div className="w-full h-[calc(100vh-200px)] flex flex-col justify-center items-center">
       <h1 className="text-5xl font-bold">Home</h1>
